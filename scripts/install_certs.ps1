7f4f2bcc2652e661cbb30f43135de3e6c2f34886
Austin Keller <austin.keller@smartsheet.com>
2021-06-05 15:56:05 -0700

    Make a version for hosting my gaming/minecraft server

--- scripts/install_certs.ps1
+++
@@ -1,5 +0,0 @@
-certutil -addstore -f "ROOT" C:\Users\vagrant\data\apollo-ca.pem
-certutil -addstore -f "ROOT" C:\Users\vagrant\data\smartsheet-root
-certutil -addstore -f "ROOT" C:\Users\vagrant\data\smartsheet-root-x2
-certutil -addstore -f "ROOT" C:\Users\vagrant\data\production-root-ca.pem
-certutil -addstore -f "ROOT" C:\Users\vagrant\data\smartsheet-corporate-root-ca.pem
